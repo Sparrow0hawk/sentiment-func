381a304d5724e841b1a7ef99411420bb461b5bea
sparrow0hawk <alexjcoleman@gmail.com>
2020-10-03 13:22:06 +0100

    updated main function

--- sentiment/__init__.py
+++ sentiment/__init__.py
@@ -1,24 +1,14 @@
 import logging
-
 import azure.functions as func
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    analyzer = SentimentIntensityAnalyzer()
+    text = req.params.get("text")
+    scores = analyzer.polarity_scores(text)
+    sentiment = "positive" if scores["compound"] > 0 else "negative"
+    return func.HttpResponse(sentiment)
